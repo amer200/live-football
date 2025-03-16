4dd70da5a3b5109d578629c4fec42d69cf182ddd
amer200 <amermostafa@gmail.com>
2025-03-16 20:34:44 +0200

    user login & check Subscription& update Subscription

--- controller/user.js
+++ controller/user.js
@@ -26,3 +26,47 @@ exports.createUser = async (req, res) => {
         res.status(500).json({ message: "خطأ في السيرفر", error: error });
     }
 }
+exports.logIn = async (req, res) => {
+    try {
+        const { phone, password } = req.body;
+        const user = await User.findOne({ phone: phone });
+        if (!user || !bcrypt.compareSync(password, user.password)) return res.status(400).json({ message: "رقم الهاتف او كلمة المرور خطاء!" });
+        if (new Date(user.subscriptionExpiresAt).getTime() < Date.now()) return res.status(400).json({ message: "انتهت مدة الاشتراك تواص مع الادارة للتجديد!" })
+        const u = {
+            id: user._id,
+            name: user.name,
+            phone: user.phone,
+            subscriptionExpiresAt: user.subscriptionExpiresAt,
+            rolle: "user"
+        }
+        const token = jwt.sign({
+            exp: Math.floor(Date.now() / 1000) + (3 * 60 * 60),
+            data: { user: u },
+        }, process.env.ACCESS_TOKEN);
+        return res.status(200).json({
+            msg: "ok",
+            token: token
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "خطأ في السيرفر", error: error });
+    }
+}
+exports.updateExtendSubscription = async (req, res) => {
+    try {
+        const { userId, durationInDays } = req.body;
+        const user = await User.findById(userId);
+        if (!user) return res.status(400).json({ message: "المستخدم غير موجود" });
+        const expiresAt = new Date();
+        expiresAt.setDate(expiresAt.getDate() + durationInDays);
+        user.subscriptionExpiresAt = expiresAt;
+        await user.save();
+        return res.status(200).json({
+            msg: "ok",
+            data: user.subscriptionExpiresAt
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "خطأ في السيرفر", error: error });
+    }
+}
--- routes/user.js
+++ routes/user.js
@@ -3,4 +3,6 @@ const routes = express.Router();
 const userController = require("../controller/user");
 const isAdmin = require("../middlewares/admin").isAuth;
 routes.post("/create-user", isAdmin, userController.createUser);
+routes.post("/log-in", userController.logIn);
+routes.post("/update-subscription", isAdmin, userController.updateExtendSubscription);
 module.exports = routes;

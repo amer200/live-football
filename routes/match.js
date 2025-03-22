52934bca60a5918c975a814be4275f0b4ae9b0a6
amer200 <amermostafa@gmail.com>
2025-03-22 21:59:44 +0200

    admin auth

--- routes/match.js
+++ routes/match.js
@@ -3,10 +3,10 @@ const routes = express.Router();
 const matchController = require("../controller/match");
 const isAdmin = require("../middlewares/admin").isAuth;
 
-routes.post('/add-categ', matchController.addCateg);
-routes.post('/edit-categ', matchController.editCateg);
-routes.get('/remove-categ/:id', matchController.removeCateg);
-routes.get('/get-all-categ', matchController.getAllCateg);
-routes.get('/get-categ-by-name/:name', matchController.getCategByName);
-routes.get('/get-categ-by-id/:id', matchController.getCategById);
+routes.post('/add-categ', isAdmin, matchController.addCateg);
+routes.post('/edit-categ', isAdmin, matchController.editCateg);
+routes.get('/remove-categ/:id', isAdmin, matchController.removeCateg);
+routes.get('/get-all-categ', isAdmin, matchController.getAllCateg);
+routes.get('/get-categ-by-name/:name', isAdmin, matchController.getCategByName);
+routes.get('/get-categ-by-id/:id', isAdmin, matchController.getCategById);
 module.exports = routes;
